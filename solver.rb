d7cda2cf751172efd6605108a4c42d74f0b00ea9
dvcorreia1984 <victor@onsleer.co.za>
2023-10-28 21:30:32 +0200

    Rubocop linters

--- solver.rb
+++ solver.rb
@@ -14,20 +14,20 @@ class Solver
   end
 
   describe '#fizzbuzz' do
-  it 'returns "fizz" when the number is divisible by 3' do
-    expect(solver.fizzbuzz(6)).to eq('fizz')
-  end
+    it 'returns "fizz" when the number is divisible by 3' do
+      expect(solver.fizzbuzz(6)).to eq('fizz')
+    end
 
-  it 'returns "buzz" when the number is divisible by 5' do
-    expect(solver.fizzbuzz(10)).to eq('buzz')
-  end
+    it 'returns "buzz" when the number is divisible by 5' do
+      expect(solver.fizzbuzz(10)).to eq('buzz')
+    end
 
-  it 'returns "fizzbuzz" when the number is divisible by 3 and 5' do
-    expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
-  end
+    it 'returns "fizzbuzz" when the number is divisible by 3 and 5' do
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
 
-  it 'returns the number as a string for any other case' do
-    expect(solver.fizzbuzz(7)).to eq('7')
+    it 'returns the number as a string for any other case' do
+      expect(solver.fizzbuzz(7)).to eq('7')
+    end
   end
-end
 end
